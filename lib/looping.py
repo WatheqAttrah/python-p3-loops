ab631b4a10e88b168e71c0f82d9d21d08b8b09c2
Watheq Attrah <novaatrah@gmail.com>
2023-07-25 17:34:41 -0700

    Complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,36 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print(f"Happy New Year!")
+
+
+happy_new_year()
+
 
 def square_integers(int_list):
     # code goes here!
+    square_list = [number * number for number in int_list]
+    return square_list
     pass
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    number = 1
+    while number <= 100:
+        if (number % 3 == 0 and number % 5 == 0):
+            print("FizzBuzz")
+        elif (number % 3 == 0 and number % 5 != 0):
+            print("Fizz")
+        elif (number % 5 == 0 and number % 3 != 0):
+            print("Buzz")
+        else:
+            print(number)
+        number += 1
+
+
+fizzbuzz()
